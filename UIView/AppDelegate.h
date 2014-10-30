b74f4e7ba9e056cc473c9e1acc22f4371314c6e3
hx_leichunxiang <apple@leichunxiang.local>
2014-10-30 19:27:09 +0800

    Initial Commit

---
+++ UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by hx_leichunxiang on 14-10-30.
+//  Copyright (c) 2014年 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by hx_leichunxiang on 14-10-30.
+//  Copyright (c) 2014年 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
